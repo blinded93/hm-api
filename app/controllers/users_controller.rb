8f5bd670b2ba9d76792f67cc9d5697178d8be24b
Kyle Hughes <blinded93@gmail.com>
2019-12-01 15:33:12 -0800

    add shared controller actions to verify/auth user actions. add basic user actions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+	def index
+		render json: User.all
+	end
+
+	def show
+		user = User.find_by_id(params[:id])
+		render json: user
+	end
+
+	def create
+		user = User.new(user_params)
+
+		user.save if user.valid?
+		status = user.errors.any? ? 400 : 200
+
+		render json: user, status: status
+	end
+
+	private
+		def user_params
+			params.permit(:username, :email, :password)
+		end
 end
