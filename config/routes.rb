93675963fa6de09bacd98971a58357454117ae7d
Kyle Hughes <blinded93@gmail.com>
2019-12-01 15:36:31 -0800

    add auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+	post '/auth_user' => 'authentication#authenticate_user'
+	post '/verify_user' => 'authentication#verify_user'
+  resources :users
   resources :expenses
   resources :items
   resources :tasks
   resources :lists
   resources :properties
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
